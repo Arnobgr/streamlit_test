7685f7db3a89a3ffcdbe8375c4ac7af6ccbf23aa
Arno <90406956+Arnobgr@users.noreply.github.com>
2023-06-08 20:59:03 +0200

    Update ta_streamlit_app.py

--- ta_streamlit_app.py
+++ ta_streamlit_app.py
@@ -1,155 +1,133 @@
-# Imports
-import datetime
-import pandas as pd
+# imports
 import yfinance as yf
-import cufflinks as cf
 import streamlit as st
+import datetime 
+import pandas as pd
+import cufflinks as cf
 from plotly.offline import iplot
 
+## set offline mode for cufflinks
 cf.go_offline()
 
-# Define function to get list of S&P500 companies
+# data functions
 @st.cache
 def get_sp500_components():
     df = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
     df = df[0]
-    tickers = df['Symbol'].to_list()
+    tickers = df["Symbol"].to_list()
     tickers_companies_dict = dict(
-        zip(df['Symbol'], df['Security'])
+        zip(df["Symbol"], df["Security"])
     )
     return tickers, tickers_companies_dict
 
-
-# Define function to load data for a specific symbol
 @st.cache
 def load_data(symbol, start, end):
     return yf.download(symbol, start, end)
 
-
-# Define function to save data as csv
 @st.cache
 def convert_df_to_csv(df):
-    return df.to_csv().encode("utf=8")
+    return df.to_csv().encode("utf-8")
 
+# sidebar
 
-# Define the sidebar to select the tickers and the dates
+## inputs for downloading data
 st.sidebar.header("Stock Parameters")
 
 available_tickers, tickers_companies_dict = get_sp500_components()
 
 ticker = st.sidebar.selectbox(
-    "Ticker",
-    available_tickers,
+    "Ticker", 
+    available_tickers, 
     format_func=tickers_companies_dict.get
 )
 start_date = st.sidebar.date_input(
-    "Start date",
-    datetime.date(2020, 1, 1)
+    "Start date", 
+    datetime.date(2019, 1, 1)
 )
 end_date = st.sidebar.date_input(
-    "End date",
+    "End date", 
     datetime.date.today()
 )
 
 if start_date > end_date:
     st.sidebar.error("The end date must fall after the start date")
 
-
-# Define the part of the sidebar used to tune the details of the TA
+## inputs for technical analysis
 st.sidebar.header("Technical Analysis Parameters")
 
 volume_flag = st.sidebar.checkbox(label="Add volume")
 
-
-# Add the expander with parameters of the SMA
 exp_sma = st.sidebar.expander("SMA")
-sma_flag = st.sidebar.checkbox(label="Add SMA")
-sma_periods = exp_sma.number_input(
-    label="SMA Periods",
-    min_value=1,
-    max_value=50,
-    value=20,
+sma_flag = exp_sma.checkbox(label="Add SMA")
+sma_periods= exp_sma.number_input(
+    label="SMA Periods", 
+    min_value=1, 
+    max_value=50, 
+    value=20, 
     step=1
 )
 
-
-# Add the expander with the parameters of the Bollinger Bands
 exp_bb = st.sidebar.expander("Bollinger Bands")
-bb_flag = st.sidebar.checkbox(label="Add Bollinger Bands")
-bb_periods = exp_bb.number_input(
-    label="BB Periods",
-    min_value=1,
-    max_value=50,
-    value=20,
-    step=1
-)
-bb_std = exp_bb.number_input(
-    label="# of standard deviations",
-    min_value=1,
-    max_value=4,
-    value=2,
-    step=1
-)
+bb_flag = exp_bb.checkbox(label="Add Bollinger Bands")
+bb_periods= exp_bb.number_input(label="BB Periods", 
+                                min_value=1, max_value=50, 
+                                value=20, step=1)
+bb_std= exp_bb.number_input(label="# of standard deviations", 
+                            min_value=1, max_value=4, 
+                            value=2, step=1)
 
-
-#Add the expander with parameters of the RSI
-exp_rsi = st.sidebar.expander("RSI")
-rsi_flag = st.sidebar.checkbox(label="Add RSI")
-rsi_periods = exp_rsi.number_input(
-    label="RSI Periods",
-    min_value=1,
-    max_value=50,
-    value=20,
+exp_rsi = st.sidebar.expander("Relative Strength Index")
+rsi_flag = exp_rsi.checkbox(label="Add RSI")
+rsi_periods= exp_rsi.number_input(
+    label="RSI Periods", 
+    min_value=1, 
+    max_value=50, 
+    value=20, 
     step=1
 )
-rsi_upper = exp_rsi.number_input(
-    label="RSI Upper",
-    min_value=50,
-    max_value=90,
-    value=70,
-    step=1
-)
-rsi_lower = exp_rsi.number_input(
-    label="RSI Lower",
-    min_value=10,
-    max_value=50,
-    value=30,
-    step=1
-)
+rsi_upper= exp_rsi.number_input(label="RSI Upper", 
+                                min_value=50, 
+                                max_value=90, value=70, 
+                                step=1)
+rsi_lower= exp_rsi.number_input(label="RSI Lower", 
+                                min_value=10, 
+                                max_value=50, value=30, 
+                                step=1)
 
+# main body
 
-# Specify the title and some information in the app's main body
-st.title("Basic web app for technical analysis")
+st.title("A simple web app for technical analysis")
 st.write("""
 ### User manual
-* You can select any company from the S&P500
+* you can select any of the companies that is a component of the S&P index
+* you can select the time period of your interest
+* you can download the selected data as a CSV file
+* you can add the following Technical Indicators to the plot: Simple Moving 
+Average, Bollinger Bands, Relative Strength Index
+* you can experiment with different parameters of the indicators
 """)
 
-
-#Load the historical stock prices
 df = load_data(ticker, start_date, end_date)
 
-
-# Add the expander with a preview of the downloaded data
+## data preview part
 data_exp = st.expander("Preview data")
 available_cols = df.columns.tolist()
 columns_to_show = data_exp.multiselect(
-    "Columns",
-    available_cols,
+    "Columns", 
+    available_cols, 
     default=available_cols
 )
 data_exp.dataframe(df[columns_to_show])
 
 csv_file = convert_df_to_csv(df[columns_to_show])
 data_exp.download_button(
-    label="Download as CSV",
+    label="Download selected as CSV",
     data=csv_file,
-    file_name=f'{ticker}_stock_prices.csv',
-    mime="text/csv"
+    file_name=f"{ticker}_stock_prices.csv",
+    mime="text/csv",
 )
 
-
-# Create the candlestick chart with the selected TA indicators
+## technical analysis plot
 title_str = f"{tickers_companies_dict[ticker]}'s stock price"
 qf = cf.QuantFig(df, title=title_str)
 if volume_flag:
@@ -157,7 +135,8 @@ if volume_flag:
 if sma_flag:
     qf.add_sma(periods=sma_periods)
 if bb_flag:
-    qf.add_bollinger_bands(periods=bb_periods, boll_std=bb_std)
+    qf.add_bollinger_bands(periods=bb_periods,
+                           boll_std=bb_std)
 if rsi_flag:
     qf.add_rsi(periods=rsi_periods,
                rsi_upper=rsi_upper,
